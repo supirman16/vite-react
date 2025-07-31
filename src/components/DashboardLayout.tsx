3de7f27498f05e3213dc8f80a34c89e3968742f4
Samuel Simangunsong <samuel.simangunsong93@gmail.com>
2025-08-01 01:14:14 +0700

    Update DashboardLayout.tsx

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { AppContext, AppContextType } from '../App';
 import Header from './Header';
 import Navigation from './Navigation';
