645eff987206610103b799c255c4e5b4bf1ab56d
Samuel Simangunsong <samuel.simangunsong93@gmail.com>
2025-08-01 01:17:48 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { AppContext, AppContextType } from '../App';
 import { LogOut, Sun, Moon, Menu } from 'lucide-react';
 
