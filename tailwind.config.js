de63621fc5ceaa6d165f0b2a449bafcbb971b9cc
Samuel Simangunsong <samuel.simangunsong93@gmail.com>
2025-08-02 15:59:59 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,12 +22,17 @@ export default {
         'slide-up-fade': {
             'from': { opacity: '0', transform: 'translateY(10px)' },
             'to': { opacity: '1', transform: 'translateY(0)' },
+        },
+        'slide-in': { // <-- Animasi baru
+          'from': { transform: 'translateX(-100%)' },
+          'to': { transform: 'translateX(0)' },
         }
       },
       animation: {
         'fade-in': 'fade-in 0.3s ease-out forwards',
         'fade-in-fast': 'fade-in-fast 0.2s ease-out forwards',
         'slide-up-fade': 'slide-up-fade 0.5s ease-out forwards',
+        'slide-in': 'slide-in 0.3s ease-out forwards', // <-- Animasi baru
       }
     },
   },
